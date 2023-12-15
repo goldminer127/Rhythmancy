8b4bbc970c5fd62adf4200293c1dffedb37070c0
goldminer127 <goldenplaymc@yahoo.com>
2023-12-15 00:37:20 -0500

    Item can open UI now

--- src/client/java/rhythmancy/tempo/RhythmancyClient.java
+++ src/client/java/rhythmancy/tempo/RhythmancyClient.java
@@ -3,15 +3,18 @@ package rhythmancy.tempo;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rhythmancy.tempo.gui.ExampleGUI;
 import rhythmancy.tempo.gui.ExampleScreen;
+import rhythmancy.tempo.items.RhythmancyItem;
 
 public class RhythmancyClient implements ClientModInitializer {
+	public static final String MOD_ID = "rhythmancy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	private static KeyBinding keyBinding;
 	@Override
 	public void onInitializeClient() {
@@ -27,5 +30,6 @@ public class RhythmancyClient implements ClientModInitializer {
 				client.setScreen(new ExampleScreen(new ExampleGUI()));
 			}
 		});
+		RhythmancyItem.registerModItems();
 	}
 }
--- src/client/java/rhythmancy/tempo/items/RhythmancyItem.java
+++ src/client/java/rhythmancy/tempo/items/RhythmancyItem.java
@@ -4,15 +4,15 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
-import rhythmancy.tempo.Rhythmancy;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import rhythmancy.tempo.RhythmancyClient;
 
-public class RhythmancyItems {
+public class RhythmancyItem {
     public static final Item TRIANGLE = registerItem("triangle", new Item(new FabricItemSettings()));
-    public static final Item GUIItem = registerItem("guiitem", new Item(new FabricItemSettings()));
+    public static final Item GUIItem = registerItem("guiitem", new GuiItem(new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(TRIANGLE);
@@ -23,13 +23,12 @@ public class RhythmancyItems {
     }
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(Rhythmancy.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, new Identifier(RhythmancyClient.MOD_ID, name), item);
     }
-
     public static void registerModItems() {
-        Rhythmancy.LOGGER.info("Registering Rhythmancy Items");
+        RhythmancyClient.LOGGER.info("Registering Rhythmancy Items");
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((RhythmancyItems::addItemsToIngredientItemGroup));
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register((RhythmancyItems::addItemsToFunctionalItemGroup));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((RhythmancyItem::addItemsToIngredientItemGroup));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register((RhythmancyItem::addItemsToFunctionalItemGroup));
     }
 }
