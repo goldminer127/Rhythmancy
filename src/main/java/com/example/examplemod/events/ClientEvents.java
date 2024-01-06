4ada28bf22f3a9f98446a27e681f20d95d392623
goldminer127 <goldenplaymc@yahoo.com>
2024-01-06 02:33:17 -0500

    Added hud activation via item filtration

--- src/main/java/com/example/examplemod/events/ClientEvents.java
+++ src/main/java/com/example/examplemod/events/ClientEvents.java
@@ -2,21 +2,26 @@ package com.example.examplemod.events;
 
 import com.example.examplemod.ExampleMod;
 import com.example.examplemod.screens.TestHud;
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterGuiOverlaysEvent;
+import net.neoforged.neoforge.client.event.RenderGuiOverlayEvent;
+import org.slf4j.Logger;
 
 public class ClientEvents
 {
+    private static final Logger LOGGER = LogUtils.getLogger();
     @Mod.EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModBusEvents
     {
         @SubscribeEvent
         public static void registerOverlays(RegisterGuiOverlaysEvent event)
         {
-            event.registerAboveAll(new ResourceLocation(ExampleMod.MODID, "textures/hud/bar.png"), TestHud.HUD_BAR);
+            event.registerAboveAll(new ResourceLocation(ExampleMod.MODID), TestHud.HUD_BAR);
         }
     }
 }
