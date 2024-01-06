6c9ec3c18fb770ba33973fe572e5309dbe1434ce
goldminer127 <goldenplaymc@yahoo.com>
2024-01-05 23:36:44 -0500

    modified things

---
+++ src/main/java/com/example/examplemod/screens/TestHud.java
@@ -0,0 +1,15 @@
+package com.example.examplemod.screens;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.client.gui.overlay.IGuiOverlay;
+
+public class TestHud
+{
+    private static final ResourceLocation BAR = new ResourceLocation(ExampleMod.MODID, "textures/bar.png");
+    /*
+    public static final IGuiOverlay HUD_BAR = (((gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
+
+    });
+    */
+}
