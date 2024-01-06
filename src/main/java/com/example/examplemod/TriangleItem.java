9208a6bd44450b57b6a706d541ee2a57b6a83125
Joshua Angulo <jangulo@albany.edu>
2024-01-06 03:00:09 -0500

    Removing unused imports

--- src/main/java/com/example/examplemod/TriangleItem.java
+++ src/main/java/com/example/examplemod/TriangleItem.java
@@ -1,13 +1,11 @@
 package com.example.examplemod;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import org.jetbrains.annotations.NotNull;
 
